f169923a88d7ce4564283cb35cf709de743cffd2
Dmitry <zmey.task@gmail.com>
2025-02-01 14:50:00 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by lorenc_D_K on 01.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterRest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by lorenc_D_K on 01.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
