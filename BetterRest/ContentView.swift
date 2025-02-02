bfb27b8416b33940406a23b5ae4245465b6a4617
Dmitry <zmey.task@gmail.com>
2025-02-02 21:43:37 +0300

    Add stepper

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var wakeup = Date.now
+    
+    @State private var sleepAmount = 8.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            DatePicker("Please enter a date", selection: $wakeup, in: Date.now...)
+                .labelsHidden()
+            
+            Stepper("We have to sleep \(sleepAmount.formatted()) hours",
+                    value: $sleepAmount,
+                    in: 4...12,
+                    step: 0.25
+            )
         }
         .padding()
     }
